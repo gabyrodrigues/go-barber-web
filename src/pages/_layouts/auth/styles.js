0a591c4ca4f9d3ffe0b0556c898d6bf0d1896287
Gabriely Rodrigues <gabriely.bleez@gmail.com>
2020-04-05 18:15:04 -0300

    autenticação: formulários e validações

--- src/pages/_layouts/auth/styles.js
+++ src/pages/_layouts/auth/styles.js
@@ -34,6 +34,13 @@ export const Content = styled.div`
             }
         }
 
+        span {
+            color: #fb6f91;
+            align-self: flex-start;
+            margin: 0 0 10px;
+            font-weight: 700;
+        }
+
         button {
             margin: 5px 0 0;
             height: 44px;
