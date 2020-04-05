58daab559b03256b077ade8431951920bea8a575
Gabriely Rodrigues <gabriely.bleez@gmail.com>
2020-04-05 20:30:29 -0300

    autenticação e guardando dados do  perfil

--- src/routes/Route.js
+++ src/routes/Route.js
@@ -5,12 +5,14 @@ import { Route, Redirect } from 'react-router-dom';
 import AuthLayout from '../pages/_layouts/auth';
 import DefaultLayout from '../pages/_layouts/default';
 
+import store from '../store/';
+
 export default function RouteWrapper({
     component: Component, //component(atributo component da rota): Component(componente passado como parametro)
     isPrivate, //utilizado em rotas em que só é possível acessar se o usuário estiver logado
     ...rest //todos as outras propriedades são colocadas aqui
 }) {
-    const signed = false; //o padrão é não logado
+    const { signed } = store.getState().auth; //o padrão é não logado (false)
 
     if(!signed && isPrivate) {
         return <Redirect to="/" />
