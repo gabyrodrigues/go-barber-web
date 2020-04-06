a7ba7643720305adf90c8cb5ab436daf96170efe
Gabriely Rodrigues <gabriely.bleez@gmail.com>
2020-04-05 22:50:00 -0300

    cadastro + token de autenticação nas requisições

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -22,6 +22,8 @@ export function* signIn({ payload }){
             return;
         }
 
+        api.defaults.headers.Authorization = `Bearer ${token}`;
+
         yield put(signInSuccess(token, user));
 
         history.push('/dashboard');
@@ -31,6 +33,40 @@ export function* signIn({ payload }){
     }
 }
 
+export function* signUp({ payload }) {
+    try {
+        const { name, email, password } = payload;
+        
+        yield call(api.post, 'users', {
+            name,
+            email,
+            password,
+            provider: true
+        });
+
+        history.push('/'); //depois do cadastro é redirecionado à tela de login
+    } catch (err) {
+        toast.error('Falha do cadastro. Verifique seus dados e tente novamente');
+
+        yield put(signFailure());
+    }
+}
+
+export function setToken({ payload }) {
+    //se for a primeira vez que o usuário estiver acessando a aplicação
+    if(!payload) {
+        return; 
+    }
+
+    const { token } = payload.auth;
+
+    if(token) { //token sempre presente quando o usuário estiver logado, então toda chamada api envia o token de autenticação junto com a chamada
+        api.defaults.headers.Authorization = `Bearer ${token}`;
+    }
+}
+
 export default all([
-    takeLatest('@auth/SIGN_IN_REQUEST', signIn)
+    takeLatest('persist/REHYDRATE', setToken),
+    takeLatest('@auth/SIGN_IN_REQUEST', signIn),
+    takeLatest('@auth/SIGN_UP_REQUEST', signUp)
 ]);
